80b8c4067e4eb31ee54a2abeea4c7b4f2ff3106a
mikershov94 <ershovme@gmail.com>
2020-01-27 15:00:18 +0800

    Configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const fs = require('fs');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
-    src: path.join(__dirname, '../src'),
-    dist: path.join(__dirname, '../dist')
+    src: path.join(__dirname, './src'),
+    dist: path.join(__dirname, './dist')
 };
+
+const PAGES_DIR = PATHS.src;
+const PAGES = fs.readdirSync(PAGES_DIR).filter((filename) => filename.endsWith('.pug'));
 
 module.exports = {
     
@@ -15,5 +22,88 @@ module.exports = {
         filename: 'js/[name].js',
         path: PATHS.dist
     },
+
+    devServer: {
+        contentBase: path.join(__dirname, './public'),
+        open: true,
+        overlay: true,
+        stats: 'errors-only',
+        port: 3000
+    },
+
+    module: {
+        rules: [
+
+            {
+                test: /\.pug$/,
+                use: [
+                    { loader: 'pug-loader' }
+                ]
+            },
+
+            {
+                test: /\.sass$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { 
+                            sourceMap: true,
+                            config: { path: __dirname }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+
+            {
+                test: /\.(png|gif|jpe?g)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+
+            {
+                test: /\.svg$/,
+                loader: 'url-loader'
+            },
+
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+
+        ],
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/main.css',
+        }),
+        
+        ...PAGES.map((page) => new HtmlWebpackPlugin({
+            template: `${PAGES_DIR}/${page}`,
+            filename: `./${page.replace(/\.pug$/, '.html')}`
+        }))
+    ],
 
 }
